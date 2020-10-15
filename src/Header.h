fc331863dc9c474af6c1c107b16c3cdd9df684ee
Gerald <geraldo.febrian12@gmail.com>
2020-10-15 15:27:00 +0700

    edit

---
+++ src/Header.h
@@ -0,0 +1,14 @@
+#ifndef HEADER_H // To make sure you don't declare the function more than once by including the header multiple times.
+
+#define HEADER_H
+#define GLFW_INCLUDE_NONE
+#include <GLFW/glfw3.h>
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+#include "glad/glad.h"
+#include "linmath.h"
+
+int render();
+
+#endif // !HEADER_H
